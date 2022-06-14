01e8c64cf255b9aa24daf5904fa71a9d31fe0bd8
ben <jammydude7@googlemail.com>
2022-06-14 10:17:24 +0100

    adjusted positioning of pad elements

--- js/animations.js
+++ js/animations.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({
-    defaults: { duration: .5, ease: "Power3.easeOut" },
+    defaults: { duration: 0.5, ease: "Power3.easeOut" },
 });
 
 const tl2 = gsap.timeline({
@@ -8,21 +8,25 @@ const tl2 = gsap.timeline({
 
 const launch = document.querySelector(".launch-button");
 
-// launch sequence 
+// launch sequence
 launch.addEventListener("click", () => {
-    tl.to('.start', {opacity: 0});
+    tl.to(".start", { opacity: 0 });
     tl.set(".start", { display: "none" });
-    tl.fromTo('.launch-pad', {opacity: 0}, {opacity: 1}, "<");
-    tl.to('.countdown', {opacity: 0, delay: .5});
-    tl.set('.countdown', {innerText: "2", opacity: 1});
-    tl.to('#flame2', {opacity: 1}, "<");
-    tl.set('#flame1', {opacity: 1}, "<");
-    tl2.fromTo('#smoke', {opacity:0 }, {opacity: 1, delay: 2.2});
-    tl.to('.countdown', {opacity: 0, delay: .5});
-    tl.set('.countdown', {innerText: "1", opacity: 1, duration: 1});
-    tl.to('.countdown', {opacity: 0, delay: 1});
-    tl.set('.countdown', {innerText: "Huston we have a problem", opacity: 1});
-    tl.to('#flame1', {opacity: 0});
-    tl.to('#flame2', {opacity: 0});
-    tl2.to('#smoke', {opacity: 0});
-  });
+    tl.fromTo(".launch-pad", { opacity: 0 }, { opacity: 1 }, "<");
+    tl.to(".countdown", { opacity: 0, delay: 0.5 });
+    tl.set(".countdown", { innerText: "2", opacity: 1 });
+    tl.to("#flame2", { opacity: 1 }, "<");
+    tl.set("#flame1", { opacity: 1 }, "<");
+    tl2.fromTo("#smoke", { opacity: 0 }, { opacity: 1, delay: 2.2 });
+    tl.to(".countdown", { opacity: 0, delay: 0.5 });
+    tl.set(".countdown", { innerText: "1", opacity: 1, duration: 1 });
+    tl.to(".countdown", { opacity: 0, delay: 1 });
+    tl.set(".countdown", {
+        innerText: "Huston we have a problem",
+        opacity: 1,
+        delay: 0.5,
+    });
+    tl.to("#flame1", { opacity: 0 });
+    tl.to("#flame2", { opacity: 0 });
+    tl2.to("#smoke", { opacity: 0 });
+});
--- js/rocket-smoke.js
+++ js/rocket-smoke.js
@@ -3,102 +3,101 @@ let options = {
     spread: 25,
     puff_size: 100,
     speed: 1.5,
-    puff_increase: .2,
-    dissipation: .005
-  };
-  
-  
-//   Create canvas.
-  var canvas3 = document.querySelector('#smoke');
-  const ctx = canvas3.getContext('2d');
-  document.body.appendChild(canvas3);
+    puff_increase: 0.2,
+    dissipation: 0.005,
+};
+
+// Create canvas.
+var canvas3 = document.querySelector("#smoke");
+const ctx = canvas3.getContext("2d");
 //   Set canvas to the size of the viewable window.
-  canvas3.width = window.innerWidth;
-  canvas3.height = window.innerHeight;
-  
-  const origin = [
-    canvas.width/2,
-    canvas.height/2
-  ];
-  let particles = [];
-  
-  
-  // Draw loop.
-  drawing();
-  
-  function drawing () {
-    particles.push(new Particle({
-      pos: [origin[0] + rand(-10, 10), origin[1]],
-      size: 1,
-      speed: options.speed,
-      color: '#CCC',
-      borderColor: 'rgba(0,0,0,0)',
-      angle: rand(180 + (90-options.spread), 360 - (90-options.spread))
-    }));
+canvas3.width = window.innerWidth;
+canvas3.height = window.innerHeight;
+
+const origin = [canvas.width / 2, canvas.height / 2];
+let particles = [];
+
+// Draw loop.
+drawing();
+
+function drawing() {
+    particles.push(
+        new Particle({
+            pos: [origin[0] + rand(-10, 10), origin[1]],
+            size: 1,
+            speed: options.speed,
+            color: "#CCC",
+            borderColor: "rgba(0,0,0,0)",
+            angle: rand(
+                180 + (90 - options.spread),
+                360 - (90 - options.spread)
+            ),
+        })
+    );
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particles = particles.filter(particle => {
-      particle.move();
-      particle.size += options.puff_increase;
-      particle.alpha -= options.dissipation;
-      particle.draw();
-      if (particle.pos[0] > canvas.width
-         || particle.pos[1] > canvas.height
-         || particle.size > options.puff_size
-         || particle.alpha < .01) {
-        return false;
-      }
-      return true;
+    particles = particles.filter((particle) => {
+        particle.move();
+        particle.size += options.puff_increase;
+        particle.alpha -= options.dissipation;
+        particle.draw();
+        if (
+            particle.pos[0] > canvas.width ||
+            particle.pos[1] > canvas.height ||
+            particle.size > options.puff_size ||
+            particle.alpha < 0.01
+        ) {
+            return false;
+        }
+        return true;
     });
-    
-    
-    
+
     setTimeout(() => {
-      window.requestAnimationFrame(drawing);
+        window.requestAnimationFrame(drawing);
     }, 1);
-  }
-  
-  function Particle (options) {
+}
+
+function Particle(options) {
     this.angle = 0;
     this.curve = 0;
-    this.pos = [0,0];
+    this.pos = [0, 0];
     this.size = 100;
     this.speed = 1;
-    this.color = 'rgba(255,64,64,.95)';
+    this.color = "rgba(255,64,64,.95)";
     this.borderColor = this.color;
     this.waveX = false;
     this.waveY = false;
     this.index = 0;
-    this.alpha = .5;
-    
+    this.alpha = 0.5;
+
     // Override defaults.
     for (var i in options) {
-      this[i] = options[i];
+        this[i] = options[i];
     }
-    
+
     this.move = function () {
-      this.angle += this.curve;
-      var radians = this.angle*Math.PI/180;
-      this.pos[0] += Math.cos(radians)*this.speed,
-      this.pos[1] += Math.sin(radians)*this.speed;
-      
-      if (this.waveX) {
-        this.pos[0] += Math.cos(this.index);
-      }
-      if (this.waveY) {
-        this.pos[1] += Math.sin(this.index);
-      }
-    }
+        this.angle += this.curve;
+        var radians = (this.angle * Math.PI) / 180;
+        (this.pos[0] += Math.cos(radians) * this.speed),
+            (this.pos[1] += Math.sin(radians) * this.speed);
+
+        if (this.waveX) {
+            this.pos[0] += Math.cos(this.index);
+        }
+        if (this.waveY) {
+            this.pos[1] += Math.sin(this.index);
+        }
+    };
     this.draw = function () {
-      ctx.globalAlpha = this.alpha || .00001;
-      ctx.strokeStyle = this.borderColor;
-      ctx.fillStyle = this.color;
-      ctx.beginPath();
-      ctx.arc(this.pos[0],this.pos[1],this.size,0,2*Math.PI);
-      ctx.stroke();
-      ctx.fill();
-    }
-  }
-  
-  function rand(min, max) {
-    return Math.random()*(max-min)+min;
-  }
+        ctx.globalAlpha = this.alpha || 0.00001;
+        ctx.strokeStyle = this.borderColor;
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.pos[0], this.pos[1], this.size, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    };
+}
+
+function rand(min, max) {
+    return Math.random() * (max - min) + min;
+}
